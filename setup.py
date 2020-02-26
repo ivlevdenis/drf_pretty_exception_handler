bdac848eccdf71238db69e44973a7268fb79cae6
Ivlev Denis <di-erz@yandex.ru>
2020-02-26 20:34:10 +0300

    Remove autodeploy to pypi

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
     install_requires=requirements,
     license="MIT license",
     long_description=readme,
+    long_description_content_type='text/markdown',
     include_package_data=True,
     keywords='drf_pretty_exception_handler',
     name='drf_pretty_exception_handler',
