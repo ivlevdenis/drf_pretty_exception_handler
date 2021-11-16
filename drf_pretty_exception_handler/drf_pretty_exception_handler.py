5e8116fa1efed180561029721b8b5c2fc729d44c
Denis Ivlev <di-erz@evrone.com>
2021-11-16 11:38:43 +0300

    Add PRETTY_STANDARD_PYTHON_EXCEPTIONS setting

--- drf_pretty_exception_handler/drf_pretty_exception_handler.py
+++ drf_pretty_exception_handler/drf_pretty_exception_handler.py
@@ -1,11 +1,19 @@
+from django.conf import settings
 from django.core.exceptions import PermissionDenied
 from django.http import Http404
 from rest_framework import exceptions
+from rest_framework.views import exception_handler as default_exception_handler
 from rest_framework.response import Response
 from rest_framework.views import set_rollback
 
+DEFAULT_SETTINGS = {"PRETTY_STANDARD_PYTHON_EXCEPTIONS": True}
+
+SETTINGS = {**DEFAULT_SETTINGS, **getattr(settings, "PRETTY_EXCEPTION_HANDLER", {})}
+PRETTY_STANDARD_PYTHON_EXCEPTIONS = SETTINGS.get("PRETTY_STANDARD_PYTHON_EXCEPTIONS")
+
 
 def exception_handler(exc, context):
+
     if isinstance(exc, Http404):
         exc = exceptions.NotFound()
     elif isinstance(exc, PermissionDenied):
@@ -28,9 +36,8 @@ def exception_handler(exc, context):
 
         set_rollback()
         return Response(data, status=exc.status_code, headers=headers)
-    else:
+    elif PRETTY_STANDARD_PYTHON_EXCEPTIONS:
         data = {'status_code': 500, 'errors': {}}
         data['errors']['non_field_errors'] = [f'{exc.__class__.__name__}: {exc}']
         return Response(data, status=500)
-
     return None
